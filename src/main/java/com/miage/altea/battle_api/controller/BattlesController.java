4a35e00d60d5836a2c84738be9ab42be45e8db43
mathieu <msansen@gmail.com>
2020-03-21 14:38:55 +0100

    WIP add system.properties ans crossOrigin

--- src/main/java/com/miage/altea/battle_api/controller/BattlesController.java
+++ src/main/java/com/miage/altea/battle_api/controller/BattlesController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value="/battles", produces = MediaType.APPLICATION_JSON_VALUE)
 public class BattlesController {
